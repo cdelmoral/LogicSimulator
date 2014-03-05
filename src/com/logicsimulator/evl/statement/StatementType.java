f0992ccc770734f762c8602e7a2e2affaa9f05ad
cdelmoral <carderomad@gmail.com>
2014-03-05 00:15:33 -0600

    Added documentation

---
+++ src/com/logicsimulator/evl/statement/StatementType.java
@@ -0,0 +1,9 @@
+package com.logicsimulator.evl.statement;
+
+/**
+ * Type of statement. It can be <code>MODULE</code>, <code>WIRE</code>, <code>COMPONENT</code>,
+ * <code>ASSIGN</code> or <code>ENDMODULE</code>.
+ */
+public enum StatementType {
+	MODULE, WIRE, COMPONENT, ASSIGN, ENDMODULE
+}
